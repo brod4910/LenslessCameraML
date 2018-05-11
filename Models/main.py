10252cfdbfb823fa4f4dd74122c58a53f0ba9fea
BrianRod <brod4910@gmail.com>
2018-05-11 17:15:49 -0600

    changes the dimensions the conv net allows

--- Models/main.py
+++ Models/main.py
@@ -30,7 +30,7 @@ feature_layers = {
 classifier_layers = {
 	'1': [['L', 256 * 40 * 30, 1024], ['D'], ['FC', 1024, 5]],
 	'2': [['L', 512 * 10 * 10, 1024], ['D'], ['L', 1024, 2048], ['D'], ['FC', 2048, 10]],
-	'3': [['L', 512 * 10 * 10, 2048], ['D'], ['L', 2048, 4096], ['D'], ['FC', 4096, 10]]
+	'3': [['L', 512 * 7 * 7, 2048], ['D'], ['L', 2048, 4096], ['D'], ['FC', 4096, 10]]
 }
 
 def CreateArgsParser():
@@ -50,7 +50,7 @@ def CreateArgsParser():
 					help='dimensions of both height and width to be resized')
 	parser.add_argument('--num-processes', type=int, default=2, metavar='N',
 	                help='how many training processes to use (default: 2)')
-	parser.add_argument('--lr_scheduler', action='store_true')
+	parser.add_argument('--lr-scheduler', action='store_true')
 	parser.add_argument('--root-dir', required= True, help='root directory where enclosing image files are located')
 	parser.add_argument('--train-csv', required= True, help='path to the location of the training csv')
 	parser.add_argument('--test-csv', required= True, help='path to the location of the test csv')
