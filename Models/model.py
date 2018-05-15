137235d27568df8c117ce20d0b922459a1ddffef
BrianRod <brod4910@gmail.com>
2018-05-14 22:37:35 -0600

    Adds model 7. Adds the ability to change the Activation Function. Adds the ability to use Alpha Dropout and adds in 1D batch norm after linear layers

--- Models/model.py
+++ Models/model.py
@@ -25,10 +25,14 @@ def make_layers(layout):
 		elif layer[0] == 'C':
 			conv2d = nn.Conv2d(in_channels= layer[1], out_channels= layer[2], 
 				kernel_size= (layer[3][0], layer[3][1]), stride= layer[4], padding= layer[5])
-			if layer[6] is True and layer[7] is True:
+			if layer[6] == 'ReLU':
+				layers += [conv2d, nn.BatchNorm2d(layer[2]), nn.ReLU(inplace=True)]
+			elif layer[6] == 'PReLU':
+				layers += [conv2d, nn.BatchNorm2d(layer[2]), nn.PReLU(inplace=True)]
+			elif layer[6] == 'SELU':
 				layers += [conv2d, nn.BatchNorm2d(layer[2]), nn.SELU(inplace=True)]
-			elif layer[6] is True and layer[7] is not True:
-				layers += [conv2d, nn.SELU(inplace= True)]
+			elif layer[6] == 'LeakyReLU':
+				layers += [conv2d, nn.BatchNorm2d(layer[2]), nn.LeakyReLU(inplace=True)]
 			else:
 				layers += [conv2d]
 
@@ -38,9 +42,18 @@ def make_classifier_layers(layout):
 	layers = []
 	for layer in layout:
 		if layer[0] == 'L':
-			layers += [nn.Linear(layer[1], layer[2]), nn.SELU(inplace= True)]
+			if layer[3] == 'ReLU':
+				layers += [nn.Linear(layer[1], layer[2]), nn.BatchNorm1d(layer[2]), nn.ReLU(inplace= True)]
+			elif layer[3] == 'PReLU':
+				layers += [nn.Linear(layer[1], layer[2]), nn.BatchNorm1d(layer[2]), nn.PReLU(inplace= True)]
+			elif layer[3] == 'SELU':
+				layers += [nn.Linear(layer[1], layer[2]), nn.BatchNorm1d(layer[2]), nn.SELU(inplace= True)]
+			elif layer[3] == 'LeakyReLU':
+				layers += [nn.Linear(layer[1], layer[2]), nn.BatchNorm1d(layer[2]), nn.LeakyReLU(inplace= True)]
 		elif layer[0] == 'D':
 			layers += [nn.Dropout(layer[1])]
+		elif layer[0] == 'AD':
+			layers+= [nn.AlphaDropout(layer[1])]
 		elif layer[0] == 'FC':
 			layers += [nn.Linear(layer[1], layer[2])]
 
